222cdb0710636f30b04d913d3f83f50a3ba11ef0
Marlos Ribeiro <mgrb@cesar.school>
2024-04-08 11:35:30 -0300

    feat: AVT 04. Q06 - add encontrar menor numero par informado

--- src/ATV04/q06.py
+++ src/ATV04/q06.py
@@ -3,6 +3,8 @@
 Faça um algoritmo que encontre qual o maior número par digitado pelo usuário. O
 usuário deve digitar 10 números e ao final o algoritmo deve imprimir o
 resultado.
+Faça também um algoritmo que encontre qual o menor número par digitado pelo
+usuário.
 """
 
 from communs.util import print_header
@@ -10,12 +12,16 @@ from communs.util import print_header
 if __name__ == '__main__':
     print_header('Maior número par')
 
-    maior: int = 0
+    maior: int = None
+    menor: int = None
 
     for i in range(10):
         num = int(input(f'Digite o {i+1}º número: '))
-
-        if num % 2 == 0 and num > maior:
-            maior = num
+        if num % 2 == 0:
+            if maior is None or num > maior:
+                maior = num
+            if menor is None or num < menor:
+                menor = num
 
     print(f'Maior número par: {maior}.')
+    print(f'Menor número par: {menor}.')
